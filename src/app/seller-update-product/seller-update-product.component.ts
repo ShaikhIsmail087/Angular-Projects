db8639c9946740723a9df4a2ad8017cb7d57c79b
Ismail Shaikh <ismail_shaikh@ildc-inlp.idealake.com>
2023-10-09 15:23:29 +0530

    third commit

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type'
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
   styleUrls: ['./seller-update-product.component.css']
 })
-export class SellerUpdateProductComponent {
+export class SellerUpdateProductComponent implements OnInit {
+  productData: undefined | product;
+  productMessage: undefined | string;
 
-  submit(data:any){}
+  constructor(private route: ActivatedRoute, private product: ProductService) { }
+
+  ngOnInit() {
+    let productId = this.route.snapshot.paramMap.get('id');
+    console.warn(productId);
+    productId && this.product.getProduct(productId).subscribe((data) => {
+      console.warn(data);
+      this.productData = data;
+    })
+  }
+
+  submit(data: product) {
+    console.warn(data);
+    if(this.productData){
+      data.id=this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productMessage = "Product has updated";
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+  }
 
 }
