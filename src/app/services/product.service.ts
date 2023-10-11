93fd7965828c2b5085cf600d7abd04a3082fa0b1
Ismail Shaikh <ismail_shaikh@ildc-inlp.idealake.com>
2023-10-11 15:13:51 +0530

    Dynamic product UI added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -35,7 +35,7 @@ export class ProductService {
   }
 
   trendyProducts(){
-    return this.http.get<product[]>('http://localhost:3000/products?_limit=8');
+    return this.http.get<product[]>('http://localhost:3000/products?_limit=9');
   }
 
 }
