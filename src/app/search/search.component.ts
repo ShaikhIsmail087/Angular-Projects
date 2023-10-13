d36394b28907bb9c0471f694ca5d9428e7f20285
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2023-10-13 09:52:19 +0530

    make dynamic search with API

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+
+  searchResult:undefined|product[];
+
+  constructor(private activeRoute:ActivatedRoute,private product:ProductService){}
+
+  ngOnInit():void{
+    let query=this.activeRoute.snapshot.paramMap.get('query');
+    console.warn(query);
+    query && this.product.searchProducts(query).subscribe((result)=>{
+      this.searchResult=result;
+    });
+  }
 
 }
