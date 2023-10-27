0f2041fbcf6458ab4743b3de0b787f035227062e
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2023-10-27 16:17:10 +0530

    Store Local Storage Cart to DB

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SignUp, login } from '../data-type';
+import { SignUp, cart, login, product } from '../data-type';
 import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -12,7 +13,7 @@ export class UserAuthComponent {
   showLogin:boolean=true;
   authError:string="";
 
-  constructor(private user:UserService) { }
+  constructor(private user:UserService,private product:ProductService) { }
 
   // ngOnInit():void {
   //   this.user.userAuthReload();
@@ -28,7 +29,9 @@ export class UserAuthComponent {
     this.user.invalidUserAuth.subscribe((result)=>{
       console.warn("Testing...",result);
       if(result){
-        this.authError="Please enter valid user details";
+        this.authError="User not found";
+      }else{
+        this.localCartToRemoteCart();
       }
       
     })
@@ -40,6 +43,34 @@ export class UserAuthComponent {
 
   openLogin(){
     this.showLogin=true;
+  }
+
+  localCartToRemoteCart(){
+    let data =localStorage.getItem('localCart');
+    if(data){
+      let cartDataList:product[]=JSON.parse(data);
+      let user =localStorage.getItem('user');
+      let userId=user && JSON.parse(user).id;
+
+      cartDataList.forEach((product:product, index)=>{
+        let cartData:cart={
+          ...product,
+          productId: product.id,
+          userId,
+        }
+        delete cartData.id;
+        setTimeout(()=>{
+          this.product.addToCart(cartData).subscribe((result)=>{
+            if(result){
+              console.warn('Item stored in DB');
+            }
+          });
+          if(cartDataList.length===index+1){
+            localStorage.removeItem('localCart'); 
+          }
+        }, 500);
+      });
+    }
   }
 
 }
