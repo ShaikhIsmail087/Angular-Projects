c506df82bc01c09dc00fced4fc739afec964abb8
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2023-10-31 12:52:41 +0530

    Done with cart page api in dynamic way

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -33,3 +33,11 @@ export interface cart {
     userId: number,
     productId: number
 }
+
+export interface priceSummary{
+    price:number,
+    discount:number,
+    tax:number,
+    delivery:number,
+    total:number
+  }
